18de98d283ce2ffd175c19c0e46bf81e36875d68
Viresh Kumar <viresh.kumar@linaro.org>
2013-07-02 16:36:28 +0530

    cpufreq: Fix serialization of frequency transitions
    Commit 7c30ed ("cpufreq: make sure frequency transitions are serialized")
    interacts poorly with systems that have a single core freqency for all
    cores.  On such systems we have a single policy for all cores with
    several CPUs.  When we do a frequency transition the governor calls the
    pre and post change notifiers which causes cpufreq_notify_transition()
    per CPU.  Since the policy is the same for all of them all CPUs after
    the first and the warnings added are generated by checking a per-policy
    flag the warnings will be triggered for all cores after the first.
    
    Fix this by allowing notifier to be called for n times. Where n is the number of
    cpus in policy->cpus.
    
    Reported-and-tested-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Git-commit: 266c13d767be61a17d8e6f2310b9b7c46278273b
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -123,7 +123,7 @@ struct cpufreq_policy {
 
 	struct kobject		kobj;
 	struct completion	kobj_unregister;
-	bool			transition_ongoing; /* Tracks transition status */
+	int			transition_ongoing; /* Tracks transition status */
 };
 
 #define CPUFREQ_ADJUST			(0)
