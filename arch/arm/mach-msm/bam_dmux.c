667a8866d1de15461cdcb587785c30170d1ec5b5
Jeffrey Carlyle <jeff.carlyle@motorola.com>
2013-08-20 16:03:06 -0500

    msm: bam_dmux: fix compilation issue
    Signed-off-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    [ljc: also call BUG instead of panic]
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/bam_dmux.c
    
    Conflicts:
    	arch/arm/mach-msm/bam_dmux.c
    
    Change-Id: Iaf3ec0fa66cdc40d2d98c2d4feeea9c13b384136
    
    Change-Id: I5633e8e3ace444f4e41d2464f8808f06a2872ab8

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -1639,6 +1639,10 @@ static void ul_timeout(struct work_struct *work)
 			ul_packet_written = 0;
 			queue_delayed_work(system_power_efficient_wq, &ul_timeout_work,
 					msecs_to_jiffies(UL_TIMEOUT_DELAY));
+		} else if (polling_mode) {
+			DMUX_LOG_KERR("%s: BAM is in polling mode, delay UL power down", __func__);
+			schedule_delayed_work(&ul_timeout_work,
+					msecs_to_jiffies(UL_TIMEOUT_DELAY));
 		} else {
 			ul_powerdown();
 		}
@@ -1664,6 +1668,7 @@ static int ssrestart_check(void)
 	if (ret == -ENODEV) {
 		DMUX_LOG_KERR("%s: modem subsystem restart failed\n", __func__);
 		dump_stack();
+		BUG();
 	}
 	return 1;
 }
