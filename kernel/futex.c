ae20305ae01d261a032ee6beb25abb9825aa1770
Dorimanx <yuri@bynet.co.il>
2014-06-07 02:37:57 +0300

    kernel/fork.c && kernel/futex.c small fixes, dual\missing defines

--- kernel/futex.c
+++ kernel/futex.c
@@ -60,8 +60,8 @@
 #include <linux/pid.h>
 #include <linux/nsproxy.h>
 #include <linux/ptrace.h>
-#include <linux/hugetlb.h>
 #include <linux/freezer.h>
+#include <linux/hugetlb.h>
 #include <linux/bootmem.h>
 
 #include <asm/futex.h>
