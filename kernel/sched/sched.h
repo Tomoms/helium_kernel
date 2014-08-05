0413cd9c1966116fbda297e9a157ddc77fbdd60a
Dorimanx <yuri@bynet.co.il>
2014-08-05 10:50:35 +0300

    sched: Add min_max_freq and rq->max_possible_freq
    rq->max_possible_freq represents the maximum frequency a cpu is
    capable of attaining, while rq->max_freq represents the maximum
    frequency a cpu can attain at a given instant. rq->max_freq includes
    constraints imposed by user or thermal driver.
    rq->max_freq <= rq->max_possible_freq.
    
    max_possible_freq is derived as max(rq->max_possible_freq) and
    represents the "best" cpu that can attain best possible frequency.
    
    min_max_freq is derived as min(rq->max_possible_freq). For homogeneous
    systems, max_possible_freq and min_max_freq will be same, while they
    could be different on heterogeneous systems.
    
    [myfluxi: revert a prior commit, caf got it right now]
    
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -12,6 +12,9 @@
 
 extern __read_mostly int scheduler_running;
 
+extern unsigned int max_possible_freq;
+extern unsigned int min_max_freq;
+
 /*
  * Helpers for converting nanosecond timing to jiffy resolution
  */
@@ -582,7 +585,11 @@ struct rq {
 	u64 max_idle_balance_cost;
 #endif
 
-	int cur_freq, max_freq, min_freq;
+	/*
+	 * max_freq = user or thermal defined maximum
+	 * max_possible_freq = maximum supported by hardware
+	 */
+	unsigned int cur_freq, max_freq, min_freq, max_possible_freq;
 	u64 cumulative_runnable_avg;
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
