88d28e67bebc6a6c7115d159aecb812a87c2a807
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-11-25 12:13:58 +0530

    sched: Fix reference to stale task_struct in try_to_wake_up()
    try_to_wake_up() currently drops p->pi_lock and later checks for need
    to notify cpufreq governor on task migrations or wakeups. However the
    woken task could exit between the time p->pi_lock is released and the
    time the test for notification is run. As a result, the test for
    notification could refer to an exited task. task_notify_on_migrate(p)
    could thus lead to invalid memory reference.
    
    Fix this by running the test for notification with task's pi_lock
    held.
    
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2477,12 +2477,13 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 stat:
 	ttwu_stat(p, cpu, wake_flags);
 
-	if (src_cpu != cpu && task_notify_on_migrate(p))
-		notify = 1;
+	if (task_notify_on_migrate(p))
+		notify = true;
+
 out:
 	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
 
-	if (task_notify_on_migrate(p)) {
+	if (notify) {
 		struct migration_notify_data mnd;
 
 		mnd.src_cpu = src_cpu;
