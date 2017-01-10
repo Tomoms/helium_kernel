b7d4d2cc7b5d0006ce1dafa2c903f45955ee1d7a
Tomoms <fondatommaso2@gmail.com>
2017-01-10 16:09:50 +0100

    state notifier: speed up driver

--- drivers/input/touchscreen/state_notifier.c
+++ drivers/input/touchscreen/state_notifier.c
@@ -76,17 +76,16 @@ EXPORT_SYMBOL_GPL(state_notifier_call_chain);
 
 static void _suspend_work(struct work_struct *work)
 {
-	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 	state_suspended = true;
+	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 	suspend_in_progress = false;
 	dprintk("%s: suspend completed.\n", STATE_NOTIFIER);
 }
 
 static void _resume_work(struct work_struct *work)
 {
-	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
-	msleep_interruptible(50);
 	state_suspended = false;
+	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 	dprintk("%s: resume completed.\n", STATE_NOTIFIER);
 }
 
@@ -98,7 +97,7 @@ void state_suspend(void)
 	dprintk("%s: suspend called.\n", STATE_NOTIFIER);
 	suspend_in_progress = true;
 
-	queue_delayed_work(susp_wq, &suspend_work, 
+	queue_delayed_work_on(0, susp_wq, &suspend_work, 
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
 
@@ -109,7 +108,7 @@ void state_resume(void)
 	suspend_in_progress = false;
 
 	if (state_suspended)
-		queue_work(susp_wq, &resume_work);
+		queue_work_on(0, susp_wq, &resume_work);
 }
 
 static int __init state_notifier_init(void)
