9465d74a26cd13776420619d18054a3e174f91f4
Tomoms <fondatommaso2@gmail.com>
2017-12-29 12:49:19 +0100

    Fix build

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -451,16 +451,6 @@ static void cpufreq_powerstats_create(unsigned int cpu,
 	powerstats->state_num = j;
 
 	snprintf(device_path, sizeof(device_path), "/cpus/cpu@%d", cpu);
-	cpu_node = of_find_node_by_path(device_path);
-	if (cpu_node) {
-		ret = of_property_read_u32_array(cpu_node, "current",
-				powerstats->curr, count);
-		if (ret) {
-			kfree(powerstats->curr);
-			kfree(powerstats);
-			powerstats = NULL;
-		}
-	}
 	per_cpu(cpufreq_power_stats, cpu) = powerstats;
 	spin_unlock(&cpufreq_stats_lock);
 }
